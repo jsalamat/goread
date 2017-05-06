b1949d018790b8d219abf6446e8deeee539af6ac
Jon Salamat <Jon Salamat>
2017-05-06 01:04:46 -0400

    got kid's book working YAY

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -10,32 +10,32 @@ router.get('/', function(req,res) {
   res.render('index');
 });
 
-router.get('/api/kids', function(req,res) {
-  // console.log("Parents");
-  query = "SELECT u.id AS kids_id, u.username, u.email FROM users AS u RIGHT JOIN parent_students AS ps ON u.id = ps.student_id WHERE ps.parent_id = ?";
-    connection.query(query, [ req.session.user_id ], function(err, kids){
-      console.log(kids);
-      var allKidsIds = "";
-      if ( kids != ""){
-        for (var k = 0; k < kids.length; k++) {
-          if (k == 0) {allKidsIds += kids[k].kids_id;
-          }else {allKidsIds += ", " + kids[k].kids_id;
-          };
-        };
+router.get('/api/kids/:valueKids', function(req,res) {
+  console.log("valueKids: " + req.params.valueKids);
+  // var query = "SELECT u.id AS kids_id, u.username, u.email FROM users AS u RIGHT JOIN parent_students AS ps ON u.id = ps.student_id WHERE ps.student_id = ?";
+  //   connection.query(query, [ req.params.valueKids ], function(err, kids){
+  //     console.log("kids: " + kids);
+  //     var allKidsIds = "";
+  //     if ( kids != ""){
+  //       for (var k = 0; k < kids.length; k++) {
+  //         if (k == 0) {allKidsIds += kids[k].kids_id;
+  //         }else {allKidsIds += ", " + kids[k].kids_id;
+  //         };
+  //       };SELECT u.id AS kids_id, u.username, u.email FROM users AS u RIGHT JOIN parent_students AS ps ON u.id = ps.student_id WHERE ps.parent_id = ?
 
-      }
-      query="SELECT ub.user_id, ub.book_id, b.title FROM user_books AS ub INNER JOIN BOOKS AS b ON ub.book_id = b.id WHERE ub.user_id = ?";
-      connection.query(query, [ allKidsIds ], function(err, kidbooks){
+  //     }
+      var query="SELECT ub.user_id, ub.book_id, b.title FROM user_books AS ub INNER JOIN BOOKS AS b ON ub.book_id = b.id WHERE ub.user_id = ?";
+      	connection.query(query, [ req.params.valueKids ], function(err, kidbooks){
         
-        console.log(kidbooks);
+        	console.log(kidbooks);
 
-        kidbooks = JSON.stringify(kidbooks);
-        kidbooks = kidbooks.replace(/&quot;/g,"\"");
-        console.log(kidbooks);
+        	kidbooks = JSON.stringify(kidbooks);
+        	kidbooks = kidbooks.replace(/&quot;/g,"\"");
+        	console.log(kidbooks);
 
-        res.json(kidbooks);
-  });
-  })
+    		res.json(kidbooks);
+  		});
+  	// })
 });
 
 module.exports = router;
--- public/assets/js/parent.js
+++ public/assets/js/parent.js
@@ -11,14 +11,35 @@ $kidbooksubmit.on('click', function(event){
 })
 console.log("here")
 
-// $.get("/api/kids")
-// 	.then(function(res) {
-// 		console.log("hehehehe")
-// 		console.log(res);
-// 		$("#kids-books").append(res);
-// 	})
+
 
 function selectKidBooks() {
 	var valueKids = document.getElementById("selectKids").value;
 	console.log(valueKids);
+	// var queryURL = "/api/kids/" + valueKids;
+	// $.ajax({
+ //    url: queryURL,
+ //    method: "GET"
+ //  }).then(function(res) 
+	$.get("/api/kids/" + valueKids)
+	.then(function(res) {
+		console.log("====================")
+		// console.log(res);
+var newTable = "";
+		$("#kids-books").empty();
+		
+			console.log(typeof res)
+			var selectedBookbyKid = JSON.parse(res);
+		selectedBookbyKid.forEach(function(item, index) {
+			console.log(item);
+		newTable += '<tr class="selectkid">';
+		newTable += '<td>' + item.user_id + '</td>';
+		newTable += '<td>' + item.title + '</td>';
+		// newTable += '<td>' + item.current_page + '</td>'  <--causing error
+		newTable += '</tr>';
+		})
+	$("#kids-books").append(newTable);
+	})
 }
+// [{"user_id":2,"book_id":8,"title":"Captain Underpants and the Big, Bad Battle of the Bionic Booger Boy"},
+// {"user_id":2,"book_id":12,"title":"Wayne Gretzky"}]
